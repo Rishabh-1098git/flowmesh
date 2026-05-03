23d7ee48be7115ea2a16ea8bdc323c5c9928c65b
unknown <rishabhsaini1098@gmail.com>
2026-05-03 16:11:40 +0530

    redis configured and added redis enqueue and dequeue functions

--- go.mod
+++ go.mod
@@ -2,4 +2,12 @@ module flowmesh
 
 go 1.26.2
 
-require github.com/lib/pq v1.12.3
+require (
+	github.com/lib/pq v1.12.3
+	github.com/redis/go-redis/v9 v9.19.0
+)
+
+require (
+	github.com/cespare/xxhash/v2 v2.3.0 // indirect
+	go.uber.org/atomic v1.11.0 // indirect
+)
---
+++ queue/redis.go
@@ -0,0 +1,27 @@
+package queue
+
+import (
+	"context"
+	"strconv"
+
+	"github.com/redis/go-redis/v9"
+)
+
+func EnqueueJob(client *redis.Client, jobID int, pipelineID int) error {
+	ctx := context.Background()
+	err := client.LPush(ctx, "jobs_queue", jobID).Err()
+	return err
+}
+
+func DequeueJob(client *redis.Client) (int, error) {
+	ctx := context.Background()
+	val, err := client.RPop(ctx, "jobs_queue").Result()
+	if err != nil {
+		return 0, err
+	}
+	jobID, err := strconv.Atoi(val)
+	if err != nil {
+		return 0, err
+	}
+	return jobID, nil
+}
